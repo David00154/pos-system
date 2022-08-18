a61a9b905e762df05c2b3caafb828bdfe08e0d92
david00154 <davidbriggs00154@gmail.com>
2022-08-19 00:59:30 +0100

    using as dashboard template

--- packages/web/tailwind.config.cjs
+++ packages/web/tailwind.config.cjs
@@ -1,28 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-	"node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
-    "./index.html",
-    "./src/**/*.{ts,tsx}",
-  ],
-  theme: {
+	content: ["node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}", "./index.html", "./src/**/*.{ts,tsx}"],
+	theme: {
 		extend: {
 			fontFamily: {
-				'space-grotesk': ['Space Grotesk', 'sans-serif']
+				"space-grotesk": ["Space Grotesk", "sans-serif"],
 			},
-			colors:{
-				'custom-dark':'rgb(24 24 24 / var(--tw-bg-opacity))',
-				'solana-dark':'#10141f',
-				'custom-white':'#e6eeff',
-				'custom-blue':'#0052ff',
-				'discord-dark':'#36393f',
-				'discord-purple':'#5865f2',
-				'discord-blue':'#00aff4',
-				'discord-dark-2':'#202225'
-			}
-		}
+			colors: {
+				"custom-dark": "rgb(24 24 24 / var(--tw-bg-opacity))",
+				"solana-dark": "#10141f",
+				"custom-white": "#e6eeff",
+				"custom-blue": "#0052ff",
+				"discord-dark": "#36393f",
+				"discord-purple": "#5865f2",
+				"discord-blue": "#00aff4",
+				"discord-dark-2": "#202225",
+				"custom-gray": "#b9bbbe",
+			},
+		},
 	},
-  plugins: [
-	require('flowbite/plugin')
-  ],
-}
+	plugins: [require("flowbite/plugin")],
+};
